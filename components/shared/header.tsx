843d782ac4b7fb61f5158d31e28edd6569d54c95
taneros <taneros@duck.com>
2025-03-23 20:38:09 +0300

    init

---
+++ components/shared/header.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+interface Props {
+  className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn('border border-button', className)}></header>
+  );
+};
