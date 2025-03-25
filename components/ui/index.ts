52dff96531119f26ce65a8d50000772bf124c76f
taneros <taneros@duck.com>
2025-03-25 20:37:35 +0300

    feat: add header

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export {Button} from './button';
+export {Input} from './input';
+export {Select} from './select';
+export {Checkbox} from './checkbox';
+export {Slider} from './slider';
+export {Popover} from './popover';
+export {Dialog} from './dialog';
+export { Drawer } from './drawer';
+export { Skeleton } from './skeleton';
