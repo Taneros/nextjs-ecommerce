44712f656eb41df761f29bc2f2665298a4993e02
taneros <taneros@duck.com>
2025-03-25 20:35:56 +0300

    fix: finish header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Container } from "@/components/shared";
 import Image from "next/image";
 import { Button } from "@/components/ui";
-import { User } from "lucide-react";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
 
 interface IHomeProps {
   className?: string;
@@ -26,6 +26,21 @@ export const Header: React.FC<IHomeProps> = ({ className }) => {
             <User size={16} />
             Войти
           </Button>
+
+          <div>
+            <Button className="group relative">
+              <b>520 Р</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="relative" size={16} strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+                size={20}
+              />
+            </Button>
+          </div>
         </div>
       </Container>
     </header>
