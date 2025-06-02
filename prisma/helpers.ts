bc48074bec64e9f94e7d44d966a0f28b59d3e36d
taneros <taneros@duck.com>
2025-06-02 21:12:16 +0300

    feat: seed dev db

---
+++ prisma/helpers.ts
@@ -0,0 +1,22 @@
+import { Prisma } from '@prisma/client';
+
+const randomDecimalNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
+};
+
+export const generateProductItem = ({
+  productId,
+  pizzaType,
+  size,
+}: {
+  productId: number;
+  pizzaType?: number;
+  size?: number;
+}) => {
+  return {
+    productId,
+    price: randomDecimalNumber(190, 600),
+    pizzaType,
+    size,
+  } satisfies Prisma.ProductItemUncheckedCreateInput;
+};
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,7 @@
-import { CATEGORIES } from "./constants";
+import { generateProductItem } from "./helpers";
+import { CATEGORIES, INGREDIENTS, PRODUCTS } from "./constants";
 import { prisma } from "./prisma-client";
-import { hashSync } from 'bcrypt'
+import { hashSync } from "bcrypt";
 
 async function up() {
   await prisma.user.createMany({
@@ -23,12 +24,101 @@ async function up() {
   });
 
   await prisma.category.createMany({
-    data: CATEGORIES
-  })
+    data: CATEGORIES,
+  });
 
+  await prisma.ingredient.createMany({
+    data: INGREDIENTS,
+  });
+  
+  await prisma.product.createMany({
+    data: PRODUCTS,
+  });
+
+    const pizza1 = await prisma.product.create({
+    data: {
+      name: 'Пепперони фреш',
+      imageUrl:
+        'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+      categoryId: 1,
+      ingredients: {
+        connect: INGREDIENTS.slice(0, 5),
+      },
+    },
+  });
+
+   const pizza2 = await prisma.product.create({
+    data: {
+      name: 'Сырная',
+      imageUrl:
+        'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+      categoryId: 1,
+      ingredients: {
+        connect: INGREDIENTS.slice(5, 10),
+      },
+    },
+  });
+
+  const pizza3 = await prisma.product.create({
+    data: {
+      name: 'Чоризо фреш',
+      imageUrl:
+        'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+      categoryId: 1,
+      ingredients: {
+        connect: INGREDIENTS.slice(10, 40),
+      },
+    },
+  });
+
+    await prisma.productItem.createMany({
+    data: [
+      // Пицца "Пепперони фреш"
+      generateProductItem({ productId: pizza1.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza1.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Сырная"
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 1, size: 40 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 20 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza2.id, pizzaType: 2, size: 40 }),
+
+      // Пицца "Чоризо фреш"
+      generateProductItem({ productId: pizza3.id, pizzaType: 1, size: 20 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 30 }),
+      generateProductItem({ productId: pizza3.id, pizzaType: 2, size: 40 }),
+
+      // Остальные продукты
+      generateProductItem({ productId: 1 }),
+      generateProductItem({ productId: 2 }),
+      generateProductItem({ productId: 3 }),
+      generateProductItem({ productId: 4 }),
+      generateProductItem({ productId: 5 }),
+      generateProductItem({ productId: 6 }),
+      generateProductItem({ productId: 7 }),
+      generateProductItem({ productId: 8 }),
+      generateProductItem({ productId: 9 }),
+      generateProductItem({ productId: 10 }),
+      generateProductItem({ productId: 11 }),
+      generateProductItem({ productId: 12 }),
+      generateProductItem({ productId: 13 }),
+      generateProductItem({ productId: 14 }),
+      generateProductItem({ productId: 15 }),
+      generateProductItem({ productId: 16 }),
+      generateProductItem({ productId: 17 }),
+    ],
+  });
 }
 
-async function down() {}
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE;`;
+  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE;`;
+}
 
 async function main() {
   try {
